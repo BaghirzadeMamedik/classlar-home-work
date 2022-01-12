cd3ae2051e28e9c60f728019c1918a8595b1f3c6
BaghirzadeMamedik <brizeforge@gmail.com>
2022-01-12 12:37:26 +0400

    Add project files.

---
+++ classlar home work/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using classlar_home_work;
+
+products product1 = new();
+
+product1.Id = 1;
+product1.Name = "Compar";
+product1.Price = 50;
+
+
+
+
+static void SEOLink(string text)
+{
+    var seo = text.ToLower().Replace(" ", "-")
+                  .Replace("ə", "e")
+                  .Replace("ğ", "g")
+                  .Replace("ç", "c");
+
+    Console.WriteLine(seo);
+}
+
+SEOLink(product1.Name);
+
+static void EDV18(int edv)
+{
+    int faiz;
+    faiz =(int)(edv * 0.18);
+    Console.WriteLine(faiz);
+}
+
+EDV18(product1.Price);
---
+++ classlar home work/products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classlar_home_work
+{
+    internal class products
+    {
+        public int Id;
+        public int Price;
+        public string Name;
+
+    }
+    
+}
